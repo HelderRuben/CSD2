a91c9c375d6b9be88f81f6d032aca8ef8c881a2f
Ruben Helder <ruubhelder@gmail.com>
2025-09-23 16:10:57 +0200

    beginning stage generateNotes func, pseudo code clarification

--- CSD2a/RhythmAlgorhythm.py
+++ CSD2a/RhythmAlgorhythm.py
@@ -1,6 +1,6 @@
 ##### VARIABLES #####
 # Amount of 16th notes in bar
-totalTime = 14
+totalTime = 14;
 # arrays with possible note-lengths
 lenOptionsLow = [];
 lenOptionsMid = [];
@@ -20,9 +20,22 @@ def generateRhythm(chopFactor):
     global noteListMid = generateNotes(lenOptionsMid);
     global noteListHigh = generateNotes(lenOptionsHigh);
 
+    chopNotes(chopFactor);
 
 #Function for filling noteArrays with the possible options
+
+# DIT IN PYTHON SYNTAX SCHRIJVEN
 def generateNotes(Options):
+    noteArray = [];
+    for i in range(totalTime):
+        #Fill array with note-lengths
+        noteArray.append = Options[random(Options.len)];
+        #Stop filling array if totalTime has been reached
+        if noteArray.sum >= totalTime:
+            i = totalTime;
+            #Capping last note so noteArray doesn't overflow
+            noteArray[-1] -= (noteArray.sum - totalTime);
+    return noteArray;
 
 #Function for chopping notes-randomisation based on Chopfactor
 def chopNotes(chopFactor):
